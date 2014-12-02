ac8db8905b5059334bb8aa1893e9bfdc5464873d
Grimonprez Jean-baptiste <grimonprez@a14p20.fil.univ-lille1.fr>
2014-12-02 09:49:08 +0100

    JBG : Ajout des constantes de connection

--- src/utils/Constantes.java
+++ src/utils/Constantes.java
@@ -1,5 +1,17 @@
 package utils;
 
 public class Constantes {
+
+	/**
+	 * Constantes de connexion a la BDD
+	 */
+	public static final String LOGIN = "TPCOO";
+
+	public static final String CONNECTION = "jdbc:hsqldb:file:bdd/ReservationSallesDeMusique;shutdown=true";
+
+	/**
+	 * Constantes de l'IHM
+	 */
 	public static final String APPLICATION_TITLE = "Réservation de salle de musique";
+
 }
