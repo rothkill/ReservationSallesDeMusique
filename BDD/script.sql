97b3f6b65f4d1248bf4fe8a3a6c3ce5f916fbd24
Declerck Rodolphe <declerck@a10p26.fil.univ-lille1.fr>
2014-12-02 11:19:10 +0100

    RDE : Création des tables utilisateur et forfait

--- BDD/script.sql
+++ BDD/script.sql
@@ -1,3 +1,23 @@
-create table test(
-	idTest varchar(10),
+drop table utilisateur;
+drop table forfait;
+
+create table forfait(
+	idforfait numeric(10) primary key,
+	date_achat Date,
+	date_fin Date,
+	temps_restant numeric(3)
 );
+
+create table utilisateur(
+	idutilisateur numeric(10) primary key,
+	nom varchar(20),
+	prenom varchar(20),
+	point_fidelite numeric(5),
+	idforfait numeric(10)
+);
+
+ALTER TABLE utilisateur
+ADD FOREIGN KEY (idforfait) 
+REFERENCES utilisateur(idforfait);
+
+shutdown;
