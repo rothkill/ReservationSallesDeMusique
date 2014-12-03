a2ae4801a20eb2d7e9a4ca88377ec9b323111bdb
jbgMIAGE <jb.grimonprez@etudiant.univ-lille1.fr>
2014-12-03 17:42:44 +0100

    JBG : Correction de ReservationDAO

--- src/dao/ReservationDAO.java
+++ src/dao/ReservationDAO.java
@@ -2,31 +2,31 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Date;
 
 import main.ConnectionDB;
 
-
 public class ReservationDAO {
-	
+
 	private Connection con = ConnectionDB.getInstance().getConnection();
-	
+
 	private static ReservationDAO SINGLETON;
-	
-	public ReservationDAO(){
+
+	public ReservationDAO() {
 	}
-	
-	public static ReservationDAO getInstance(){
-		if(SINGLETON == null){
+
+	public static ReservationDAO getInstance() {
+		if (SINGLETON == null) {
 			SINGLETON = new ReservationDAO();
 		}
 		return SINGLETON;
 	}
-	
-	public boolean creer(int idBureau, String description){
+
+	public boolean creer(int idBureau, String description) {
 		try {
-			PreparedStatement st = con.prepareStatement("insert into bureau values(?,?)");
+			PreparedStatement st = con
+					.prepareStatement("insert into bureau values(?,?)");
 			st.setInt(1, idBureau);
 			st.setString(2, description);
 			st.executeUpdate();
@@ -35,28 +35,27 @@ public class ReservationDAO {
 			System.out.println(e.getMessage());
 			return false;
 		}
-		
-		
+
 	}
-	
-	public Bureau rechercher(int idBureau){
-		Bureau bureau = null;
-		try {
-			PreparedStatement st = con.prepareStatement("select idbureau,description from bureau where idbureau = ?");
-			st.setInt(1, idBureau);
-			ResultSet rs = st.executeQuery();
-			if(rs.next() != false){
-				bureau = new Bureau(idBureau,rs.getString(2));
-			}
-			
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-		}
-		return bureau;
-		
+
+	public boolean deleteFromIdUtilisateurDate(Integer idUtilisateur, Date date) {
+		// TODO JBG RDK
+		return false;
 	}
-	
-	
-	
+
+	/**
+	 * Supprime toutes les reservations non confirmees dont la date de debut <
+	 * date actuelle
+	 * 
+	 * @return
+	 */
+	public boolean deleteAllReservationNonConfirmees() {
+		// TODO JBG RDK
+		/*
+		 * requete : delete from reservation where confirme="dateDepassee" and
+		 * date debut < curr
+		 */
+		return false;
+	}
 
 }
