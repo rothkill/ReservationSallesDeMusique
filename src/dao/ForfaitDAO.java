4d4336ccc13290fd659aef8111afb6a9256ac5dc
rothkill <rodolphe.declerck@etudiant.univ-lille1.fr>
2014-12-08 23:36:40 +0100

    RDE : MAJ ForfaitDAO

--- src/dao/ForfaitDAO.java
+++ src/dao/ForfaitDAO.java
@@ -5,6 +5,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import data.Categorie;
@@ -53,7 +55,44 @@ public class ForfaitDAO {
 	}
 
 	public boolean lier(Integer idUtilisateur, Integer idForfait) {
-		// TODO
+		Forfait forfait = ForfaitDAO.getInstance().rechercher(idForfait);
+		Date dateAchat = new Date();
+		Date dateFinValidite = ajouterMois(dateAchat,forfait.getValidite());
+		try {
+			PreparedStatement st = con
+					.prepareStatement("insert into carteforfait values(?,?,?,?,?)");
+			st.setInt(1,idUtilisateur);
+			st.setInt(2,idForfait);
+			st.setDate(3, (java.sql.Date) dateAchat);
+			st.setDate(4, (java.sql.Date) dateFinValidite);
+			st.setInt(5, forfait.getForfait());
+			st.executeUpdate();
+			return true;
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
 		return false;
+	}
+	
+	public static Date ajouterMois(Date date, int nbMois) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);;
+		cal.add(Calendar.MONTH, nbMois);
+		return cal.getTime();
+		}
+
+	private Forfait rechercher(Integer idForfait) {
+		try {
+			PreparedStatement st = con
+					.prepareStatement("select forfait,validite,idcategorie,prix from forfait where idforfait = ?");
+			st.setInt(1, idForfait);
+			ResultSet rs = st.executeQuery();
+			if(rs.next()){
+				return new Forfait(idForfait,rs.getInt(1),rs.getInt(2),CategorieDAO.getInstance().rechercher(rs.getInt(3)),rs.getInt(4));
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return null;
 	}
 }
