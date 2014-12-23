c5dbbf9eae90337cf590807ebff0646ffd4eb00a
jbgMIAGE <jb.grimonprez@etudiant.univ-lille1.fr>
2014-12-23 17:22:04 +0100

    JBG : reserv ation par clique glisse
    Reste a creer la reservation (changer le constructeur)

--- src/ihm/visualisation/ChronologieSallePanelReservation.java
+++ src/ihm/visualisation/ChronologieSallePanelReservation.java
@@ -1,5 +1,6 @@
 package ihm.visualisation;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -14,6 +15,11 @@ public class ChronologieSallePanelReservation extends ChronologieSallePanel
 		implements MouseListener {
 
 	private Utilisateur utilisateur;
+	private boolean isSelection;
+	private Reservation reservation;
+
+	private int x;
+	private int y;
 
 	public ChronologieSallePanelReservation(Salle salle,
 			List<Reservation> listeReservations, Utilisateur utilisateur) {
@@ -42,12 +48,6 @@ public class ChronologieSallePanelReservation extends ChronologieSallePanel
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
 		if (e.getY() <= 100 || e.getY() >= 750) {
 			return;
 		}
@@ -62,8 +62,70 @@ public class ChronologieSallePanelReservation extends ChronologieSallePanel
 				return;
 			}
 		}
+		isSelection = true;
+		System.out.println("selection");
+	}
 
-		// TODO JBG : reservation
+	public void paint(Graphics graphics) {
+		super.paint(graphics);
+		if (reservation != null) {
+			graphics.setColor(new Color(0, 100, 255));
+
+			String format1 = new SimpleDateFormat("HH").format(reservation
+					.getDateDebutReservation());
+			int debut = Integer.parseInt(format1);
+
+			graphics.fillRect(0, debut * 50, 300,
+					debut * 50 + reservation.getDuree() * 50);
+		}
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		if (isSelection) {
+			System.out.println("selection");
+			if (e.getY() <= 100 || e.getY() >= 750) {
+				isSelection = false;
+				return;
+			}
+
+			for (Reservation reservation : listeReservations) {
+				String format1 = new SimpleDateFormat("HH").format(reservation
+						.getDateDebutReservation());
+				int debut = Integer.parseInt(format1);
+
+				if (e.getY() >= debut * 50
+						&& e.getY() <= debut * 50 + reservation.getDuree() * 50) {
+					isSelection = false;
+					return;
+				}
+			}
+
+			int clickY = y / 50 + 9;
+			int releaseY = e.getY() / 50 + 9;
+
+			if (clickY - releaseY == 0) {
+				isSelection = false;
+				return;
+			}
+
+			if (clickY - releaseY <= 0) {
+				// TODO new reservetion
+				// reservation = new Reservation(idReservation, dateReservation,
+				// dateDebutReservation, dateFinReservation, confirmation,
+				// salle, utilisateur, tarif)
+			} else {
+				// TODO new reservetion
+			}
+
+		} else {
+			System.out.println("notselection");
+		}
+		isSelection = false;
+	}
+
+	public Reservation getReservation() {
+		return reservation;
 	}
 
 }
